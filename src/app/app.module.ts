40a1f289b8fb7333f0b818ecddad91f28cf45c8e
Cesar Manrique <cmanrique.hdc@gmail.com>
2021-02-18 19:05:28 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { PacienteComponent } from './components/paciente/paciente.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PacienteComponent,
+    LoadingComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
